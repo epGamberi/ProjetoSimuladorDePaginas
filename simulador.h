8cf21ac15452072b857dba1ab4480ecdd0bff80e
Enzo Ponte Gamberi <enzoca458@gmail.com>
2025-05-29 19:07:18 -0300

    Update simulador.h

---
+++ simulador.h
@@ -0,0 +1,10 @@
+#ifndef SIMULADOR_H
+#define SIMULADOR_H
+
+#include "estruturas.h"
+
+void inicializar_simulador(Simulador *sim, int algoritmo);
+void acessar_memoria(Simulador *sim, int endereco_virtual);
+void exibir_estatisticas(Simulador *sim);
+
+#endif
